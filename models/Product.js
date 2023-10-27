99bf0c8a478df91d6d2b2ed2779a061e0bd12583
unknown <rmaurer99@gmail.com>
2023-10-26 22:09:48 -0500

    filled out API routes and database models

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,38 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
@@ -19,5 +51,15 @@ Product.init(
     modelName: 'product',
   }
 );
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 
 module.exports = Product;
--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,16 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
@@ -16,5 +26,12 @@ Tag.init(
     modelName: 'tag',
   }
 );
+const Product = require('./Product');
+const ProductTag = require('./ProductTag');
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 
 module.exports = Tag;
